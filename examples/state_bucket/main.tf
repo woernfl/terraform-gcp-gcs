082b9ebcca7afe5e18f05bd2e55a9576bcbc7321
Florian Woerner <florian.woerner@onmyown.io>
2018-11-25 21:42:25 +0100

    Runned terraform fmt
    Signed-off-by: Florian Woerner <florian.woerner@onmyown.io>

--- examples/state_bucket/main.tf
+++ examples/state_bucket/main.tf
@@ -4,14 +4,13 @@ provider "google" {
   project     = "${var.gcp_project_id}"
 }
 
-
 module "tf_state_bucket" {
   source  = "woernfl/gcs/gcp"
-  version = "1.0.2"
+  version = "1.0.3"
 
-  gcs_bucket_name = "${var.gcs_bucket_name}"
-  gcs_bucket_location = "${var.gcs_bucket_location}"
+  gcs_bucket_name          = "${var.gcs_bucket_name}"
+  gcs_bucket_location      = "${var.gcs_bucket_location}"
   gcs_bucket_force_destroy = "${var.gcs_bucket_force_destroy}"
   gcs_bucket_storage_class = "${var.gcs_bucket_storage_class}"
-  gcs_bucket_versioning = "${var.gcs_bucket_versioning}"
+  gcs_bucket_versioning    = "${var.gcs_bucket_versioning}"
 }
--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "gcs_bucket" {
-  name     = "${var.gcs_bucket_name}"
-  location = "${var.gcs_bucket_location}"
+  name          = "${var.gcs_bucket_name}"
+  location      = "${var.gcs_bucket_location}"
   force_destroy = "${var.gcs_bucket_force_destroy}"
   storage_class = "${var.gcs_bucket_storage_class}"
 
